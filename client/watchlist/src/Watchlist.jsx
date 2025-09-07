93bb32f7aa688132e7949ecaef0ee00f6d6b2cba
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-09-07 14:05:11 -0500

    feat(Watchlist and Watching): movies can move freely between these components

--- client/watchlist/src/Watchlist.jsx
+++ client/watchlist/src/Watchlist.jsx
@@ -31,10 +31,37 @@ function Watchlist(props) { // props.JWTToken is JWT token
         getMovies();
     }, []); // TODO: needs to be refreshed when another movie is added
 
+    function handleDrop(movie) {
+        async function deleteAddMovie() {
+            try {
+                const response = await fetch("http://127.0.0.1:8000/api/delete-add-movie/", {
+                    method: "POST",
+                    headers: {
+                        "Authorization": `Bearer ${props.JWTToken}`,
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(movie),
+                });
+                const data = await response.json();
+                console.log(data.success);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
+
+        deleteAddMovie();
+    }
+
 
     // display titles from database
     return (
-        <div className="boxes">
+        <div className="boxes"
+            onDragOver={(event) => event.preventDefault()}
+            onDrop={(event) => {
+                const movie = JSON.parse(event.dataTransfer.getData("text/plain"));
+                handleDrop({...movie, to: "Watchlist"});
+            }}
+        >
             <div className="watchlist-header">
                 <h1>Watchlist:</h1>
                 <img onClick={handleAddClicked} src={add} style={{width: "20px", height: "20px"}}/>
